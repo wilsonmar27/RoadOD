6813eb7f6a62d4c58ab89ed9e63eb95445cd52c2
Wilson Jr <wilsonmar27@gmail.com>
2020-08-26 17:57:55 -0600

    modify fix_labels.py

--- fix_labels.py
+++ fix_labels.py
@@ -26,22 +26,21 @@ names_path = args.names
 output_dir = args.output_dir  # None if no argument specified
 
 
-def read_key(key_dir):
-    keys = dict()
+def read_key(key_dir:str, name_path:str):
     key_list = yololibs.get_lines(key_dir)
-    
+    names_list = yololibs.get_lines(name_path)
     for k1 in range(len(key_list)):
         key_list[k1] = str(key_list[k1]).split(':')
-    for k2 in range(len(key_list)):
-        keys[str(key_list[k2][0])] = str(key_list[k2][1])
-        
-    return keys
+    for k2 in range(len(names_list)):
+        key_list.append(set(names_list[k2]))       
+    return key_list
+
 
 def manage_output(output):
     out_dir = ""
     if output is None:
         out_dir = "labels_output"
-        if not os.listdir(out_dir): 
+        if not os.listdir(out_dir):
             os.makedirs(out_dir)
         else: 
             print(output)
@@ -51,7 +50,7 @@ def manage_output(output):
             elif cond == "Y" or "y":
                 os.makedirs(out_dir)
     else:
-        if os.path.exists(output) and not os.path.isfile(output): 
+        if os.path.exists(output) and not os.path.isfile(output):
             # Checking if the directory is empty or not 
             if not os.listdir(output):
                 out_dir = output
@@ -69,24 +68,38 @@ def manage_output(output):
     return out_dir
 
 
-class fixLabels:
-    def __init__(self, adir:str):
-        self.labels = yololibs.get_labels(adir)
-        self.files = [yololibs.fix_path(file_path) for file_path in iglob(os.path.join(adir, "*.txt"))]
+
+
     
-    def new_version(self, akey:dict, target_names:list, out_path:str):
-        for path in self.files:
-            if os.path.getsize(path) != 0:  # if file is empty pass
-                f = open(path, 'r')
-                lines = f.readlines()
-                f.close()
-                for line in lines:
-                    for label in self.labels:
-                        if self.labels[int(line[0])] == label:
-                            pass      
+def new_version(adir:str, out_path:str, check:set, target_names:list):
+    labels = yololibs.get_labels(adir)
+    files = [yololibs.fix_path(file_path) for file_path in iglob(os.path.join(adir, "*.txt"))]
+    for path in files:
+        file_name = path.split("/")[-1]
+        if os.path.getsize(path) != 0:  # if file is empty pass
+            f = open(os.path.join(out_path, file_name), 'w')
+            f.close()
+        else:
+            fIn = open(path, 'r')
+            lines = fIn.readlines()
+            fIn.close()
+            fOut = open(os.path.join(out_path, file_name), 'w')
+            for line in lines:
+                label = labels[int(line[0])]
+                for element in check:
+                    if label == element[0]:
+                        if len(element) == 1:
+                            fOut.write(line)
+                        elif len(element) == 2 and element[1] == 'del':
+                            pass
+                        elif len(element) == 2 and element[1] in target_names:
+                            
+                            
+                    
+                    
+                    
 
-
-key = read_key(key_path)
+directions = read_key(key_path, names_path)
 names = yololibs.get_lines(names_path)
 output = manage_output(output_dir)
 
