255e44d1626a2458fd0989bfbaa6a50a9484892d
Wilson Jr <wilsonmar27@gmail.com>
2020-09-06 15:53:11 -0600

    fixed file bug in shell scripts

--- yoloShell.sh
+++ yoloShell.sh
@@ -21,38 +21,24 @@ unzip G2dataSetV01.zip
 unzip G2datasetIMG.zip
 
 # restore dataset with original organization
-mv dataSetV01/ RoadDataSet/
-cd RoadDataSet
-mv dataSetV01/trainv1text/ trainv1/trainv1text
-mv dataSetV01/trainv2text/ trainv2/trainv2text
-mv dataSetV01/trainv3text/ trainv3/trainv3text
-mv dataSetV01/trainv4text/ trainv4/trainv4text
-mv dataSetV01/trainv5text/ trainv5/trainv5text
-mv dataSetV01/trainv6text/ trainv6/trainv6text
+mv dataSetV01/ road/
+cd road
+mv dataSetV01/trainv1text/* trainv1/
+mv dataSetV01/trainv2text/* trainv2/
+mv dataSetV01/trainv3text/* trainv3/
+mv dataSetV01/trainv4text/* trainv4/
+mv dataSetV01/trainv5text/* trainv5/
+mv dataSetV01/trainv6text/* trainv6/
 rm -r dataSetV01
-
-mv trainv1/trainv1text/*.txt trainv1/
-mv trainv2/trainv2text/*.txt trainv2/
-mv trainv3/trainv3text/*.txt trainv3/
-mv trainv4/trainv4text/*.txt trainv4/
-mv trainv5/trainv5text/*.txt trainv5/
-mv trainv6/trainv6text/*.txt trainv6/
-
-rm -r trainv1/trainv1text/
-rm -r trainv2/trainv2text/
-rm -r trainv3/trainv3text/
-rm -r trainv4/trainv4text/
-rm -r trainv5/trainv5text/
-rm -r trainv6/trainv6text/
 
 #Place dataset all in one folder
 cd ..
-mv RoadDataSet/trainv1/* RoadDataSet/
-mv RoadDataSet/trainv2/* RoadDataSet/
-mv RoadDataSet/trainv3/* RoadDataSet/
-mv RoadDataSet/trainv4/* RoadDataSet/
-mv RoadDataSet/trainv5/* RoadDataSet/
-mv RoadDataSet/trainv6/* RoadDataSet/
+mv road/trainv1/* road/
+mv road/trainv2/* road/
+mv road/trainv3/* road/
+mv road/trainv4/* road/
+mv road/trainv5/* road/
+mv road/trainv6/* road/
 
 rm -r trainv1/
 rm -r trainv2/
@@ -62,12 +48,12 @@ rm -r trainv5/
 rm -r trainv6/
 
 #split dataset train and val with 20% images to validation
-python3 ../../RoadOD/split.py RoadDataSet/ 20
-zip RoadDataSet RoadDataSetV1.zip
+python3 ../../RoadOD/split.py road/ 20
+zip road roadV1.zip
 
 cd ..
-mv data/RoadDataSet/obj/ data/
-mv data/RoadDataSet/test/ data/
+mv data/road/obj/ data/
+mv data/road/test/ data/
 
 python3 ../RoadOD/generate_set.py ./data/obj/ obj -out ./data/obj.txt
 python3 ../RoadOD/generate_set.py ./data/test/ test -out ./data/test.txt
